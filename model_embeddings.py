771ed2a8fd44328c1967678fece8ac85ba5e14c0
hiepnguyen034 <hiepnguyen034@gmail.com>
2020-05-22 14:13:56 +0700

    fix vnm embedding bugs

--- model_embeddings.py
+++ model_embeddings.py
@@ -11,8 +11,8 @@ class ModelEmbeddings(nn.Module):
     Class that converts input words to their embeddings.
  
     """
-    def __init__(self, embed_size_src, embed_size_tgt, vocab, prertrained_embed_tgt=True, 
-                prertrained_embed_src=False, 
+    def __init__(self, embed_size_src, embed_size_tgt, vocab, pretrained_embed_tgt=True, 
+                pretrained_embed_src=True, 
                 ):
         """
         Init the Embedding layers.
@@ -31,19 +31,21 @@ class ModelEmbeddings(nn.Module):
         self.source = nn.Embedding(len(vocab.src),self.embed_size_src, padding_idx = src_pad_token_idx)
         self.target = nn.Embedding(len(vocab.tgt) ,self.embed_size_tgt, padding_idx = tgt_pad_token_idx)
 
-        if prertrained_embed_tgt:
-            emb_matrix = self.get_embedding_matrix(vocab_src='target',vocab=vocab, embed_size=self.embed_size_tgt, 
+        if pretrained_embed_tgt:
+            emb_matrix_tgt = self.get_embedding_matrix(vocab_src='target',vocab=vocab, embed_size=self.embed_size_tgt, 
                                                     w2v_file= 'eng_vie_data/glove.6B.50d.txt'
                                                     )
-            self.target.weight = nn.Parameter(torch.tensor(emb_matrix,dtype=torch.float32))
+            self.target.weight = nn.Parameter(torch.tensor(emb_matrix_tgt,dtype=torch.float32))
 
             self.target.weight.requires_grad= False #freeze word2vec embedding
 
-        if prertrained_embed_src:
-            emb_matrix = self.get_embedding_matrix(vocab_src='source',vocab=vocab, embed_size=self.embed_size_src,
-                                                    w2v_file = 'wiki.vi.model.bin')
+        if pretrained_embed_src:
+            emb_matrix_src = self.get_embedding_matrix(vocab_src='source',vocab=vocab, embed_size=self.embed_size_src,
+                                                    w2v_file = 'eng_vie_data/wiki.vi.model.bin')
 
+            self.source.weight = nn.Parameter(torch.tensor(emb_matrix_src,dtype=torch.float32))
 
+            self.source.weight.requires_grad= False
 
     @staticmethod
     def open_vocab(file_path = 'eng_vn_vocab.json', src='source'):
@@ -59,8 +61,8 @@ class ModelEmbeddings(nn.Module):
 
     def get_embedding_matrix(self,vocab_src, vocab, embed_size, w2v_file):
         #open gloves
-        print(w2v_file[::-1][:4])
-        if w2v_file[::-1][:3] == 'txt':
+        print('-'*20)
+        if w2v_file[::-1][:3][::-1] == 'txt':
             with codecs.open(w2v_file,'r', encoding="cp720") as f:
                 words = set()
                 word_to_vec_map = {}
@@ -70,18 +72,19 @@ class ModelEmbeddings(nn.Module):
                     words.add(curr_word)
                     word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
 
-        elif w2v_file[::-1][3] == 'bin': #'wiki.vi.model.bin'
+        elif w2v_file[::-1][:3][::-1] == 'bin': #'wiki.vi.model.bin'
             word_to_vec_map = KeyedVectors.load_word2vec_format(w2v_file,binary=True)
 
         if vocab_src == 'target':
             emb_matrix = np.zeros((len(vocab.tgt), self.embed_size_tgt))
 
-        elif vocab == 'source':
+        elif vocab_src == 'source':
             emb_matrix = np.zeros((len(vocab.src), self.embed_size_src))
 
         for word, index in self.open_vocab(src=vocab_src).items():
             if word in word_to_vec_map:
                 emb_matrix[index, :] =word_to_vec_map[word]
 
+        print('load word embedding for {0} successfully from {1}'.format(vocab_src,w2v_file))
 
         return emb_matrix
